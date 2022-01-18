bf8739e9fb35fb7bceb8941005abd815ecfb7643
NOTIAMRAZER <77327520+NOTIAMRAZER@users.noreply.github.com>
2022-01-18 16:41:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+img = "";
+right_wristX = 0;
+right_wristY = 0;
 
 /*created by prashant shukla */
-
 var paddle2 =10,paddle1=10;
 
 var paddle1X = 10,paddle1Height = 110;
@@ -22,8 +24,21 @@ var ball = {
 }
 
 function setup(){
-  var canvas =  createCanvas(700,600);
+  canvas =  createCanvas(700, 600);
   canvas.parent('canvas');
+  video = createCapture(VIDEO);
+  video.size(700, 600);
+  //video.hide();
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(){
+
+}
+
+function modelLoaded(){
+  console.log("Model Loaded!");
 }
 
 
